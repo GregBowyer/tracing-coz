52c55280d50e4b1657268a15b44f21e375f49cdf
Greg Bowyer <gbowyer@fastmail.co.uk>
2019-09-25 17:23:22 -0700

    Initial implementation of coz↔tracing bridge

---
+++ README.md
@@ -0,0 +1,72 @@
+# tracing-coz
+
+[Rust-tracing](https://github.com/tokio-rs/tracing) support for the [`coz`
+Causal Profiler](https://github.com/plasma-umass/coz)
+
+[![Documentation](https://docs.rs/tracing-coz/badge.svg)](https://docs.rs/tracing-coz)
+
+## Usage
+
+First, follow the instructions in [`coz`] to install the `coz` command.
+
+[`coz`]: https://github.com/plasma-umass/coz/#installation
+
+Note that this crate is a facade over
+[rust-coz](https://github.com/alexcrichton/coz-rs) as such its information
+applies to this crate.
+
+With this crate `spans` are mapped to `coz::begin!` and `coz::end!`, and
+`events` are mapped to `coz::progress!`, as throughput or latency tracepoints
+respectively. More information on this [can be found
+upstream](https://github.com/plasma-umass/coz/#profiling-modes). 
+
+After you've instrumented your code, you need to also ensure that you're
+compiling with DWARF debug information. To do this you'll want to configure
+`Cargo.toml` again:
+
+```toml
+[profile.release]
+debug = 1
+```
+
+Next up you'll build your application with `cargo build --release`, and then
+finally you can run it with `coz run --- ./target/release/$your_binary`.
+
+## Caveats
+
+Known caveats so far to generate a report that collects information are:
+
+* Rust programs by default segfault when run with `coz` with an issue related to
+  [plasma-umass/coz#110](https://github.com/plasma-umass/coz/issues/110). Rust
+  programs set up a `sigaltstack` to run segfault handlers to print "you ran out
+  of stack", but this alternate stack is too small to run the `SIGPROF` handler
+  that `coz` installs. To handle this this crate provides a `coz::thread_init()`
+  function which will increase the `sigaltstack` size that Rust installs by
+  default to something large enough to run `coz`. If you see segfaults, or
+  corrupt reports, you may wish to manually call `coz::thread_init()` instead of
+  waiting for this crate to automatically call it for you, we export
+  `coz::thread_init()` for convenience here.
+
+* Debug information looks to be critical to get a report from `coz`. Make sure
+  that your program is compiled with at least line-table information (`debug =
+  1`) to ensure you get the best experience using `coz`.
+
+* Currently `coz` only works on Linux, and while this crate should compile on
+  all platforms it only actually does something on Linux.
+
+# License
+
+This project is licensed under either of
+
+ * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
+   http://www.apache.org/licenses/LICENSE-2.0)
+ * MIT license ([LICENSE-MIT](LICENSE-MIT) or
+   http://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in this project by you, as defined in the Apache-2.0 license,
+shall be dual licensed as above, without any additional terms or conditions.
---
+++ src/lib.rs
@@ -0,0 +1,105 @@
+//! Bridge between the `coz` Causal Profiler and tracing
+//!
+//! This crate takes advantage of places where tracing exposes tracepoints rust code, letting coz
+//! see these tracepoints as latency for spans and throughput for events.
+//!
+//! For usage information, consult the [`README.md` for the `coz`
+//! repository][coz-readme] as well as the [`README.md` for
+//! `coz-rs`][rust-readme].
+//!
+//! [coz-readme]: https://github.com/plasma-umass/coz/blob/master/README.md
+//! [rust-readme]: https://github.com/alexcrichton/coz-rs/blob/master/README.md
+
+// Reexport this for the times its needed
+pub use coz::{Counter, thread_init};
+
+use std::sync::atomic::{Ordering, AtomicUsize};
+
+use tracing::{
+    Id, Metadata, Event,
+    span,
+    subscriber::{self, Subscriber},
+};
+
+use chashmap::CHashMap;
+
+struct ThroughputCounter {
+    start: Counter,
+    end: Counter,
+}
+
+impl ThroughputCounter {
+    fn new(name: &'static str) -> Self {
+        ThroughputCounter {
+            start: Counter::begin(name),
+            end: Counter::end(name),
+        }
+    }
+}
+
+/// A bridge between the coz-profiler and tracing
+pub struct TracingCozBridge {
+    next_id: AtomicUsize,
+    latency_counters: CHashMap<&'static str, Counter>,
+    throughput_counters: CHashMap<Id, ThroughputCounter>,
+    idents: CHashMap<&'static str, Id>,
+}
+
+impl TracingCozBridge {
+    /// Creates a bridge between tracing and the coz profiler
+    pub fn new() -> Self {
+        TracingCozBridge {
+            next_id: AtomicUsize::new(1),
+            latency_counters: CHashMap::new(),
+            throughput_counters: CHashMap::new(),
+            idents: CHashMap::new(),
+        }
+    }
+
+    fn next_id(&self) -> Id {
+        Id::from_u64(self.next_id.fetch_add(1, Ordering::SeqCst) as u64)
+    }
+}
+
+impl Subscriber for TracingCozBridge {
+    fn register_callsite(&self, _meta: &Metadata<'_>) -> subscriber::Interest {
+        subscriber::Interest::always()
+    }
+
+    fn new_span(&self, new_span: &span::Attributes<'_>) -> Id {
+        let name = new_span.metadata().name();
+        let throughput_counters = &self.throughput_counters;
+        self.idents.upsert(name, || {
+            let next_id = self.next_id();
+            throughput_counters.upsert(next_id.clone(), || ThroughputCounter::new(name), |_| ());
+            next_id
+        }, |_| ());
+
+        (*self.idents.get(name).unwrap()).clone()
+    }
+
+    fn record_follows_from(&self, _span: &Id, _follows: &Id) {
+        // ignored
+    }
+
+    fn record(&self, _: &Id, _values: &span::Record<'_>) {
+        // ignored
+    }
+
+    fn event(&self, event: &Event<'_>) {
+        let name = event.metadata().name();
+        self.latency_counters.upsert(name, || Counter::progress(name), |cnt| cnt.increment());
+    }
+
+    fn enabled(&self, _metadata: &Metadata<'_>) -> bool {
+        true
+    }
+
+    fn enter(&self, span: &Id) {
+        self.throughput_counters.get(span).unwrap().start.increment()
+    }
+
+    fn exit(&self, span: &Id) {
+        self.throughput_counters.get(span).unwrap().end.increment()
+    }
+}
